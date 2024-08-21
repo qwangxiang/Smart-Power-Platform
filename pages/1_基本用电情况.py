e5c488fc1dd15e4dc0c96ff97ff0253b90e2ffcc
qwangxiang <qwangxiang@sjtu.edu.cn>
2024-08-21 15:15:54 +0800

    开发测试

--- pages/1_基本用电情况.py
+++ pages/1_基本用电情况.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     authenticator,name,auth_status,username = Login.Login()
     if auth_status:
         if st.button('查询数据'):
+            print('button clicked')
             data = module.GetRoomDataBetween(username, '2024-06-30 00:00:00', '2024-07-01 00:00:00')
             df = pd.DataFrame(data[0].reshape(-1,1))
             st.write(df)
